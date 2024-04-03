14ea1573f12c05161ae79772b9bcca8e86b6b795
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-04-03 16:52:50 -0600

    Completed backend main.py; Tested post with Postman

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from backend.chains.sql_chain import sql_chain_invoke
+from backend.models.sql_query import QueryInput
+from backend.utils.async_utils import async_retry
+#
+app = FastAPI(
+    title="SQL Chatbot 🤖",
+    description="Endpoints for the SQL Chatbot"
+)
+
+@async_retry(max_retries=10, delay=1)
+async def invoke_agent_with_retry(query:QueryInput) -> str:
+    print(query)
+    """Retry the agent if the tool fails to run.
+    Can happen during intermittent connection issues to external APIs."""
+    return await sql_chain_invoke(query.question, query.chat_history)
+
+@app.get("/")
+async def get_status():
+    return {"status":"running"}
+
+@app.post("/sql_chain")
+async def sql_agent(query: QueryInput) -> str:
+    response = await invoke_agent_with_retry(query)
+    return response
